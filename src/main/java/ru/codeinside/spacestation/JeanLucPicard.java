44dcdfad23090b5706e6dd586603cf3f686fb2e7
avpetin <33812531+avpetin@users.noreply.github.com>
2017-11-20 00:24:02 +0300

    Update JeanLucPicard.java
    Изменил файл JeanLucPicard

--- src/main/java/ru/codeinside/spacestation/JeanLucPicard.java
+++ src/main/java/ru/codeinside/spacestation/JeanLucPicard.java
@@ -3,6 +3,7 @@ package ru.codeinside.spacestation;
 public class JeanLucPicard implements Captain {
 
     private final Spaceship spaceship;
+    public static int roomsNumber = 0;
 
     public JeanLucPicard(Spaceship spaceship) {
         this.spaceship = spaceship;
@@ -10,7 +11,41 @@ public class JeanLucPicard implements Captain {
 
     @Override
     public int stateroomsNumber() {
-        // todo: implement logic here
-        return 0;
+    	int nCurIndex;
+    	spaceship.current().turnOn();//Зажигаем свет в первой каюте
+    	do{
+	    	//Ищем первую каюту с выключенным светом
+    		roomsNumber = 0;
+	    	do {
+	    		spaceship.next();
+	    		roomsNumber++;
+	    	}
+	    	while(spaceship.current().isLightOn() == false);
+	    	
+	    	//Выключаем свет в каюте
+	    	spaceship.current().turnOff();
+	    	
+	    	nCurIndex = roomsNumber;
+	    	//Возвращаемся в первую каюту
+	        do {
+	        	spaceship.previous();
+	        	nCurIndex--;
+	        }
+	        while (nCurIndex != 0);
+	    }
+        while(spaceship.current().isLightOn() == true);
+    	
+        return roomsNumber;
     }
-}
+    
+    public static void main(String args[]) {
+     	BypassEnterpriseTest EntTest = new BypassEnterpriseTest();
+    	try{
+    		EntTest.howManyStaterooms();
+    	}
+    	catch(Exception exc){
+    		System.out.println("Error");
+    		System.out.println(exc.getMessage()); 		
+    	}
+    }
+} 
